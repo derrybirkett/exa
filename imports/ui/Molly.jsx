dc83bf14b9536fcce51fb90f6911adce661f55b8
Design Machinas <design@DesignMac1.local>
2016-04-04 17:25:47 +0200

    init plus

---
+++ imports/ui/Molly.jsx
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+ 
+export default class Molly extends Component {
+
+  render() {
+
+    var divStyle = {
+      height: '400px', 
+      width: '400px', 
+      position: 'relative' 
+    };
+
+    return (
+
+      <div style={divStyle} className ='viewer_3Dmoljs' data-pdb='2POR' data-backgroundcolor='0xffffff' data-style='stick'></div>
+
+    );
+  }
+ 
+}
+ 
+Molly.propTypes = {
+  // This component gets the task to display through a React prop.
+  // We can use propTypes to indicate it is required
+  //mol: PropTypes.object.isRequired,
+};
